0af6076fd4f9a6a152e17daad957150f00938510
Valentin Koskin <>
2022-10-06 19:42:13 +0400

    init

---
+++ appProject/appProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appProject
+//
+//  Created by Валентин Коскин on 06.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
